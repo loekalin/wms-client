9e6c3f50b98974ab3e3b0c8dbc02ef3fef41ebd1
Dimas Avrian Fachriza <dimasavrian346@gmail.com>
2023-05-17 10:21:20 +0700

    [+] Loading Handler User

--- src/Pages/Master/Access/Partials/Table.jsx
+++ src/Pages/Master/Access/Partials/Table.jsx
@@ -3,6 +3,7 @@ import { BiSearch } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import useSWR from "swr";
+import { TailSpin } from "react-loader-spinner";
 
 const Table = () => {
   const navigate = useNavigate();
@@ -14,29 +15,35 @@ const Table = () => {
   };
   return (
     <div className="overflow-x-auto">
-      <table className="table table-compact w-full">
-        <thead>
-          <tr>
-            {tableRow.map((data, i) => {
-              return <th key={i}>{data}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((data, i) => {
-            return (
-              <tr key={i}>
-                <th className="font-medium">{i + 1}</th>
-                <th className="font-medium">{data.username}</th>
-                <th className="font-medium">{data.role}</th>
-                <td>
-                  <BiSearch onClick={doEdit} size={20} />
-                </td>
+      {isLoading ? (
+        <TailSpin color="black" wrapperClass="flex w-full p-5 items-center justify-center" />
+      ) : (
+        <>
+          <table className="table table-compact w-full">
+            <thead>
+              <tr>
+                {tableRow.map((data, i) => {
+                  return <th key={i}>{data}</th>;
+                })}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {data.map((data, i) => {
+                return (
+                  <tr key={i}>
+                    <th className="font-medium">{i + 1}</th>
+                    <th className="font-medium">{data.username}</th>
+                    <th className="font-medium">{data.role}</th>
+                    <td>
+                      <BiSearch onClick={doEdit} size={20} />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 };
