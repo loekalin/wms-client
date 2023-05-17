5fbf642e97ea1fd632b8df7f074b8566705d03c1
Dimas Avrian Fachriza <dimasavrian346@gmail.com>
2023-05-17 10:14:20 +0700

    [+] Add + VIew User

--- src/Pages/Master/Access/Partials/Table.jsx
+++ src/Pages/Master/Access/Partials/Table.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { BiSearch } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import useSWR from "swr";
 
 const Table = () => {
   const navigate = useNavigate();
-  const data = ["No", "USERNAME", "ACCESS", "DETAIL"];
+  const fetcher = (url) => axios.get(url).then((res) => res.data);
+  const { data, error, isLoading } = useSWR(`${import.meta.env.VITE_API_APP_URL}/api/users`, fetcher);
+  const tableRow = ["No", "USERNAME", "ACCESS", "DETAIL"];
   const doEdit = () => {
     navigate("/master/access/edit");
   };
@@ -13,28 +17,24 @@ const Table = () => {
       <table className="table table-compact w-full">
         <thead>
           <tr>
-            {data.map((data, i) => {
+            {tableRow.map((data, i) => {
               return <th key={i}>{data}</th>;
             })}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>1</th>
-            <td>Kaos Oblong</td>
-            <td> Kaos</td>
-            <td>
-              <BiSearch onClick={doEdit} size={20} />
-            </td>
-          </tr>
-          <tr>
-            <th>2</th>
-            <td>Kaos Oblong</td>
-            <td> Kaos</td>
-            <td>
-              <BiSearch onClick={doEdit} size={20} />
-            </td>
-          </tr>
+          {data.map((data, i) => {
+            return (
+              <tr key={i}>
+                <th className="font-medium">{i + 1}</th>
+                <th className="font-medium">{data.username}</th>
+                <th className="font-medium">{data.role}</th>
+                <td>
+                  <BiSearch onClick={doEdit} size={20} />
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
