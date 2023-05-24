ad5d9ea28f29de75c338584ac72a99791fd3c9c6
Dimas Avrian Fachriza <dimasavrian346@gmail.com>
2023-05-24 15:00:51 +0700

    [+] Loading Handler Chart

--- src/Pages/Dashboard/Partials/Chart.jsx
+++ src/Pages/Dashboard/Partials/Chart.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { LineChart, Line, CartesianGrid,Tooltip, XAxis, YAxis, ResponsiveContainer } from "recharts";
+import { LineChart, Line, CartesianGrid, Tooltip, XAxis, YAxis, ResponsiveContainer } from "recharts";
 import axios from "axios";
 import useSWR from "swr";
+import { CirclesWithBar } from "react-loader-spinner";
 
 const ChartComponent = () => {
   const fetcher = (url) => axios.get(url).then((res) => res.data);
@@ -13,20 +14,23 @@ const ChartComponent = () => {
   console.log(dataMapped);
   const dataChart = dataMapped?.name?.map((data, index) => ({
     name: data,
-    value: dataMapped.uv[index]
+    value: dataMapped.uv[index],
   }));
-  return (
+
+  return isLoading ? (
+    <CirclesWithBar color="#5c9ee4" wrapperClass="flex w-full translate-y-52 p-5 items-center justify-center" />
+  ) : (
     <ResponsiveContainer
       width="100%"
       height={500}
       className="bg-white shadow-[1px_3px_8px_rgba(0,0,0,0.25)] rounded-lg mt-12"
     >
       <LineChart width={600} height={300} data={dataChart}>
-        <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+        <Line type="linear" dataKey="value" stroke="#5c9ee4" />
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip/>
+        <Tooltip />
       </LineChart>
     </ResponsiveContainer>
   );
